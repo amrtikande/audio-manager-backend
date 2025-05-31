66434a7bb54cf4dc9c1bb41de3e32d338492dde6
amrtikande <143693117+amrtikande@users.noreply.github.com>
2025-05-31 13:58:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
